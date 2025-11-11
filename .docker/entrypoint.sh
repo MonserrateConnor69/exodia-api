00898b87e66e43ca0c7c25935bad0bb1a1216e6c
MonserrateConnor69 <mrgobasco@tip.edu.ph>
2025-11-11 17:19:55 +0800

    500 error

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+set -e # Exit immediately if a command exits with a non-zero status.
 
 # Run database migrations
+echo "Running database migrations..."
 php artisan migrate --force
 
 # Start supervisord
+echo "Starting supervisord..."
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
