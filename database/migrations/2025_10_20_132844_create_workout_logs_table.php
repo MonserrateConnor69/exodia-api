2ef27fd95faab811965ace7c086dba550fbeca7b
MonserrateConnor69 <mrgobasco@tip.edu.ph>
2025-11-11 17:30:49 +0800

    workout logs

--- database/migrations/2025_10_20_132844_create_workout_logs_table.php
+++ database/migrations/2025_10_20_132844_create_workout_logs_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
     Schema::create('workout_logs', function (Blueprint $table) {
         $table->id();
         $table->foreignId('user_id')->constrained()->onDelete('cascade');
-        $table->foreignId('user_id')->constrained(); 
         $table->foreignId('exercise_id')->constrained(); 
         $table->timestamps();
     });
