b20282f66394a17d9f023e2a06ebb49071e67667
MonserrateConnor69 <mrgobasco@tip.edu.ph>
2025-11-12 08:42:34 +0800

    pls

--- app/Models/WorkoutLog.php
+++ app/Models/WorkoutLog.php
@@ -14,19 +14,18 @@ class WorkoutLog extends Model
      *
      * @var array<int, string>
      */
-    
     protected $fillable = [
         'user_id',
         'exercise_id',
+        'recovery_stage',  // critical for your controller
+        'created_at',      // if you manually set timestamps
     ];
 
-    
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-   
     public function exercise()
     {
         return $this->belongsTo(Exercise::class);
